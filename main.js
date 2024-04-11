76a4c85e1c6bcdad795bcf93a321a98183e33e69
Vktr22 <nds.viktoria@gmail.com>
2024-04-11 09:17:50 +0200

    urlap összeállítva, fgv.js+listakezelo.js létreh

--- main.js
+++ main.js
@@ -1,51 +1,14 @@
-import { emberekLISTA } from "./adat.js";
+/* 
+jelenítsük meg az adatainkat egy táblázatban az adatok div-ben,az urlap divben legyen egy űrlap,
+amivel ilyen adatokat tudunk a táblázatba beletenni
 
-for (let index = 0; index < emberekLISTA.length; index++) {
-    const element = emberekLISTA[index];
-    console.log(element)
-}
+1.) ha a táblázat fejlécére kattintunk, akk azon mező szerint
+tudjuk rendezni a táblázatot
+2.) tudjuk törölni a táblázat adott sorát.
+3.) legyen egy szűrőmező, ahova beírva szavakat tudunk szűrni név szerint.
 
-/* adatLISTA.forEach((element, index) => {
-    adatLISTA.forEach()
-}); */
+Milyen függvények kellenek?
 
-// első element jelenti a listának az elemeit minden esetben
-emberekLISTA.forEach(function(ember, y){
-    console.log(ember.nev,y)
-    console.log(y)
-})
-
-/* emberekLISTA.forEach((element, index)=>{
-    console.log(element)
-    console.log(index)
-}) */
-
-const FERFIAK=emberekLISTA.filter(function(ember){ // (ember)=>
-    return ember.nem===false  // !ember.nem  
-})
-console.log(FERFIAK)
-
-//azokat az embereket szeretném, akik 20 évnél fiatalabbak
-const FIATALOK= emberekLISTA.filter(function(ember){
-    return ember.kor<20
-})
-console.log(FIATALOK)
-
-// 10 évnél idősebb férfiak
-const FERFIAK10FELETT = emberekLISTA.filter((ember)=>{
-    return ember.kor>10 && !ember.nem //ember.nem===false
-})
-
-//RENDEZÉS
-//  rendezzük fordított sorrendbe kor szerint az emereinket
-emberekLISTA.sort(function(e1,e2){
-    return e2.kor-e1.kor
-})
-console.log(emberekLISTA)
-
-// véletlen sorrendű rendezés, vagyis kor szerinti keverés
-// véletlen számot generálok -0,5 és + 0,5 között (fele fele lesz poz vagy neg)
-emberekLISTA.sort(function(e1,e2){
-    return Math.random()-0.5
-})
-console.log(emberekLISTA)
+1. tablazatOsszeallit(lista)->txt /visszatérési értéke/ - összeállítja a html kódot egy szöveges formátumba ((a lista a paramétere a txt a visszatérési értéke))
+2. megjelenites(txt)->nincs - megjelenít egy html szöveget egy html elemben
+*/
